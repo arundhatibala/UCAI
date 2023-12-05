afcf136427548b1d48fe6ce5d4d39c4c831a36ab
davromano <davidesmail00@gmail.com>
2023-12-05 18:05:43 +0100

    Problems with imports

--- training/supervised/loop.py
+++ training/supervised/loop.py
@@ -1,10 +1,7 @@
 # main.py
 from functions import *
 import pandas as pd
-from openpyxl.workbook import Workbook
 import torch
-import torch.nn as nn
-import torch.optim as optim
 import random
 from transformers import (
     AutoModelForCausalLM,
