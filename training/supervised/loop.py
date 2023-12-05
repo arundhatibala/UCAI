f949eab5c2db44ce9badb92857cd998bcf9870e9
davromano <davidesmail00@gmail.com>
2023-12-05 17:57:36 +0100

    CUDA not working again

--- training/supervised/loop.py
+++ training/supervised/loop.py
@@ -10,12 +10,20 @@ from transformers import (
     AutoModelForCausalLM,
     AutoTokenizer,
     BitsAndBytesConfig,
-    TrainingArguments,
-    pipeline,
-    logging,
 )
+from peft import LoraConfig, PeftModel
 
 def main():
+
+    #cuda settings here (this is not working)
+    device = torch.device("cuda:0")
+    dataset = load_dataset("csv", data_files="critique_revisions.csv", split="train")
+
+    # The model from HuggingFace
+    model_name = "NousResearch/Llama-2-7b-chat-hf"
+
+    # Fine-tuned model name
+    new_model = "supervised-test"
 
     ################################################################################
     # QLoRA parameters
@@ -43,6 +51,76 @@ def main():
 
     # Activate nested quantization for 4-bit base models (double quantization)
     use_nested_quant = False
+
+    ################################################################################
+    # TrainingArguments parameters
+
+    # Output directory where the model predictions and checkpoints will be stored
+    output_dir = "./results"
+
+    # Number of training epochs
+    num_train_epochs = 1
+
+    # Enable fp16/bf16 training (set bf16 to True with an A100)
+    fp16 = False
+    bf16 = False
+
+    # Batch size per GPU for training
+    per_device_train_batch_size = 2
+
+    # Batch size per GPU for evaluation
+    per_device_eval_batch_size = 1
+
+    # Number of update steps to accumulate the gradients for
+    gradient_accumulation_steps = 1
+
+    # Enable gradient checkpointing
+    gradient_checkpointing = True
+
+    # Maximum gradient normal (gradient clipping)
+    max_grad_norm = 0.3
+
+    # Initial learning rate (AdamW optimizer)
+    learning_rate = 2e-4
+
+    # Weight decay to apply to all layers except bias/LayerNorm weights
+    weight_decay = 0.001
+
+    # Optimizer to use
+    optim = "paged_adamw_32bit"
+
+    # Learning rate schedule (constant a bit better than cosine)
+    lr_scheduler_type = "constant"
+
+    # Number of training steps (overrides num_train_epochs)
+    max_steps = -1
+
+    # Ratio of steps for a linear warmup (from 0 to learning rate)
+    warmup_ratio = 0.03
+
+    # Group sequences into batches with same length
+    # Saves memory and speeds up training considerably
+    group_by_length = True
+
+    # Save checkpoint every X updates steps
+    save_steps = 25
+
+    # Log every X updates steps
+    logging_steps = 25
+
+    ################################################################################
+    # SFT parameters
+
+    # Maximum sequence length to use
+    max_seq_length = None
+
+    # Pack multiple short examples in the same input sequence to increase efficiency
+    packing = False
+
+    # Load the entire model on the GPU 0
+    device_map = {"": 0}
+
+    #################################################################################
 
     # Load tokenizer and model with QLoRA configuration
     compute_dtype = getattr(torch, bnb_4bit_compute_dtype)
@@ -80,15 +158,17 @@ def main():
     base_model="meta-llama/Llama-2-13b-chat-hf"
 
     tokenizer = AutoTokenizer.from_pretrained(base_model, token=access_token)
+    # Load base model
     model = AutoModelForCausalLM.from_pretrained(
-        base_model,
+        model_name,
         quantization_config=bnb_config,
-        device_map="auto"
+        device_map=device_map
     )
+    model.config.use_cache = False
+    model.config.pretraining_tp = 1
     
-    torch.cuda.empty_cache()
-    torch.cuda.memory_allocated()
-    model.cuda()
+    print(torch.cuda.empty_cache())
+    print(torch.cuda.memory_allocated())
     n_red_team_questions=len(questions)
 
     # create a DF to convert to csv and store final Critiqued-revised answers
