058cf1d61653e2e64137ebb9758079a27942a08c
davromano <davidesmail00@gmail.com>
2023-12-05 20:01:17 +0100

    Test device1

--- training/supervised/supervised_training.py
+++ training/supervised/supervised_training.py
@@ -16,7 +16,7 @@ from trl import SFTTrainer
 def main():
 
     #cuda settings here (this is not working)
-    device = torch.device("cuda:0")
+    device1 = torch.device("cuda:1")
     dataset = load_dataset("csv", data_files="critique_revisions.csv", split="train")
 
     # The model from HuggingFace
@@ -194,7 +194,7 @@ def main():
         tokenizer=tokenizer,
         args=training_arguments,
         packing=packing,
-    )
+    ).to(device1)
 
     # Train model
     trainer.train()
