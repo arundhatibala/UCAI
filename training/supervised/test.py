5dc02472c5b065b26a6c61d6c2d0743875b35f00
davromano <davidesmail00@gmail.com>
2023-12-07 13:38:54 +0100

    Test with llama

--- training/supervised/test.py
+++ training/supervised/test.py
@@ -8,7 +8,7 @@ X=torch.tensor((20,20))
 device=torch.device("cuda:0")
 X.to(device)
 
-base_model="TinyLlama/TinyLlama-1.1B-Chat-v0.6"
+base_model="TNousResearch/Llama-2-7b-chat-hf"
 
 model = AutoModelForCausalLM.from_pretrained(
         base_model
