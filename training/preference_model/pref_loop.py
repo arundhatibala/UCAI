6aa5c464308a37a7a3024fe0708278fbdf59800b
davromano <davidesmail00@gmail.com>
2023-12-07 11:27:31 +0100

    pref loop fixed maybe

--- training/preference_model/pref_loop.py
+++ training/preference_model/pref_loop.py
@@ -1,6 +1,6 @@
 # main.py
 import os
-from datasets import load_dataset
+# from datasets import load_dataset
 from functions import *
 import pandas as pd
 import torch
@@ -9,7 +9,7 @@ import random
 from transformers import (
     AutoModelForCausalLM,
     AutoTokenizer,
-    BitsAndBytesConfig,
+    # BitsAndBytesConfig,
     HfArgumentParser,
     TrainingArguments,
     pipeline,
@@ -55,12 +55,12 @@ def main():
     # Load tokenizer and model with QLoRA configuration
     compute_dtype = getattr(torch, bnb_4bit_compute_dtype)
 
-    bnb_config = BitsAndBytesConfig(
-        load_in_4bit=use_4bit,
-        bnb_4bit_quant_type=bnb_4bit_quant_type,
-        bnb_4bit_compute_dtype=compute_dtype,
-        bnb_4bit_use_double_quant=use_nested_quant,
-    )
+    # bnb_config = BitsAndBytesConfig(
+    #     load_in_4bit=use_4bit,
+    #     bnb_4bit_quant_type=bnb_4bit_quant_type,
+    #     bnb_4bit_compute_dtype=compute_dtype,
+    #     bnb_4bit_use_double_quant=use_nested_quant,
+    # )
 
     # Check GPU compatibility with bfloat16
     """if compute_dtype == torch.float16 and use_4bit:
@@ -104,35 +104,37 @@ def main():
     with open('../../prompts/good_principles.json') as json_file:
         principles = json.load(json_file)
         
-        for initial_prompt in questions:
-        # generating initial responses
-            response1 =  ask_prompt(model, tokenizer, initial_prompt)
-            response2 =  ask_prompt(model, tokenizer, initial_prompt)
+    for initial_prompt in questions:
+    # generating initial responses
+        response1 =  ask_prompt(model, tokenizer, initial_prompt)
+        response2 =  ask_prompt(model, tokenizer, initial_prompt)
 
-            answers = f"\n1. \"{r1_text}\"\n2. \"{r2_text}\"\n"
+        answers = f"\n1. \"{r1_text}\"\n2. \"{r2_text}\"\n"
 
-            r1_text = response1["choices"][0]["text"].replace(initial_prompt, "")
-            r2_text = response2["choices"][0]["text"].replace(initial_prompt, "")
-    
-            ai_generated_data = []
+        r1_text = response1["choices"][0]["text"].replace(initial_prompt, "")
+        r2_text = response2["choices"][0]["text"].replace(initial_prompt, "")
 
-    for principle in principles:
-        system_prompt="SYSTEM: You are the ASSISTANT. You only take part in this conversation as the ASSISTANT. Respond concisely and short.\n"
-        prompt = system_prompt + "Consider the following question:\nHUMAN: " + initial_prompt + "\n\n" + principle + "\n" + answers + "\nSYSTEM: Please answer only by saying \"Option 1\" or \"Option 2\".\n\nAssistant: "
-        response = ask_prompt(prompt)
-        print("----Response: ", response["choices"][0]["text"][-1])
-        pref = response["choices"][0]["text"][-1]
-        # clean preference value
-        pref = pref["choices"][0]["text"].replace(r1_text, "")
-        pref = pref.replace(r2_text, "")
-        pref = pref.replace(initial_prompt, "")
-        print(pref)
-        ai_generated_data.append(pref)
+        ai_generated_data = []
+        ai_generated_data.append(initial_prompt)
+        ai_generated_data.append(r1_text)
+        ai_generated_data.append(r2_text)
+
+        for principle in principles:
+            system_prompt="SYSTEM: You are the ASSISTANT. You only take part in this conversation as the ASSISTANT. Respond concisely and short.\n"
+            prompt = system_prompt + "Consider the following question:\nHUMAN: " + initial_prompt + "\n\n" + principle + "\n" + answers + "\nSYSTEM: Please answer only by saying \"Option 1\" or \"Option 2\".\n\nAssistant: "
+            response = ask_prompt(prompt)
+            print("----Response: ", response["choices"][0]["text"][-1])
+            pref = response["choices"][0]["text"][-1]
+            # clean preference value
+            pref = pref["choices"][0]["text"].replace(r1_text, "")
+            pref = pref.replace(r2_text, "")
+            pref = pref.replace(prompt, "")
+            print(pref)
+            ai_generated_data.append(pref)
 
     # Create a data point for the AI-generated preference dataset
         print("appended data points: ", ai_generated_data)
-
-        return ai_generated_data
+        df = df.append(pd.Series(ai_generated_data, index=df.columns), ignore_index=True)
 
 if __name__ == "__main__":
     main()
