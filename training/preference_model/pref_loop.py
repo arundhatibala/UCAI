724e5d01ede67ab0101a9e109288b0ed48988d94
davromano <davidesmail00@gmail.com>
2023-12-07 10:56:25 +0100

    pref loop updated

--- training/preference_model/pref_loop.py
+++ training/preference_model/pref_loop.py
@@ -50,74 +50,6 @@ def main():
     # Activate nested quantization for 4-bit base models (double quantization)
     use_nested_quant = False
 
-    ################################################################################
-    # TrainingArguments parameters
-
-    # Output directory where the model predictions and checkpoints will be stored
-    output_dir = "./results"
-
-    # Number of training epochs
-    num_train_epochs = 1
-
-    # Enable fp16/bf16 training (set bf16 to True with an A100)
-    fp16 = False
-    bf16 = False
-
-    # Batch size per GPU for training
-    per_device_train_batch_size = 2
-
-    # Batch size per GPU for evaluation
-    per_device_eval_batch_size = 1
-
-    # Number of update steps to accumulate the gradients for
-    gradient_accumulation_steps = 1
-
-    # Enable gradient checkpointing
-    gradient_checkpointing = True
-
-    # Maximum gradient normal (gradient clipping)
-    max_grad_norm = 0.3
-
-    # Initial learning rate (AdamW optimizer)
-    learning_rate = 2e-4
-
-    # Weight decay to apply to all layers except bias/LayerNorm weights
-    weight_decay = 0.001
-
-    # Optimizer to use
-    optim = "paged_adamw_32bit"
-
-    # Learning rate schedule (constant a bit better than cosine)
-    lr_scheduler_type = "constant"
-
-    # Number of training steps (overrides num_train_epochs)
-    max_steps = -1
-
-    # Ratio of steps for a linear warmup (from 0 to learning rate)
-    warmup_ratio = 0.03
-
-    # Group sequences into batches with same length
-    # Saves memory and speeds up training considerably
-    group_by_length = True
-
-    # Save checkpoint every X updates steps
-    save_steps = 25
-
-    # Log every X updates steps
-    logging_steps = 25
-
-    ################################################################################
-    # SFT parameters
-
-    # Maximum sequence length to use
-    max_seq_length = None
-
-    # Pack multiple short examples in the same input sequence to increase efficiency
-    packing = False
-
-    # Load the entire model on the GPU 0
-    device_map = {"": 0}
-
     #################################################################################
 
     # Load tokenizer and model with QLoRA configuration
@@ -162,7 +94,7 @@ def main():
 
     tokenizer = AutoTokenizer.from_pretrained(base_model, token=access_token)
     # Load base model
-    model1 = AutoModelForCausalLM.from_pretrained(base_model)
+    model = AutoModelForCausalLM.from_pretrained(base_model)
     
 
 
@@ -174,8 +106,8 @@ def main():
         
         for initial_prompt in questions:
         # generating initial responses
-            response1 =  ask_prompt(model1, tokenizer, initial_prompt)
-            response2 =  ask_prompt(model1, tokenizer, initial_prompt)
+            response1 =  ask_prompt(model, tokenizer, initial_prompt)
+            response2 =  ask_prompt(model, tokenizer, initial_prompt)
 
             answers = f"\n1. \"{r1_text}\"\n2. \"{r2_text}\"\n"
 
