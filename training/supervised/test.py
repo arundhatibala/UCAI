390abf37fab2a9f90ae690c0eab4c4628f9ebc6d
davromano <davidesmail00@gmail.com>
2023-12-07 13:35:17 +0100

    test 3

--- training/supervised/test.py
+++ training/supervised/test.py
@@ -1,6 +1,15 @@
 import torch 
+from transformers import (
+    AutoModelForCausalLM,
+)
 
 X=torch.tensor((20,20))
 device=torch.device("cuda:0")
 X.to(device)
 
+base_model="TinyLlama/TinyLlama-1.1B-Chat-v0.6"
+
+model = AutoModelForCausalLM.from_pretrained(
+        base_model
+    )
+model.to(device)
