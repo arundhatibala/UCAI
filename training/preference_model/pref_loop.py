e600ca55e6022f457b25b6fe0f4241acbfbf406c
davromano <davidesmail00@gmail.com>
2023-12-07 11:28:22 +0100

    Test with tinyllama for speed

--- training/preference_model/pref_loop.py
+++ training/preference_model/pref_loop.py
@@ -90,7 +90,7 @@ def main():
     questions_path='../../prompts/questions_clean.json'
     questions=load_questions(questions_path)
 
-    base_model="NousResearch/Llama-2-7b-chat-hf"
+    base_model="TinyLlama/TinyLlama-1.1B-Chat-v0.6"
 
     tokenizer = AutoTokenizer.from_pretrained(base_model, token=access_token)
     # Load base model
